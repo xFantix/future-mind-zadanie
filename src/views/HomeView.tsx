0d7f4088b997e6a02fd8c9998f1391ea4646f885
Kamil Jankowski <kamil.jankowski@profil-software.com>
2025-11-18 19:03:08 +0100

    Add components.json for configuration, implement Button component with variants, enhance HomeView with Button, and update main.css for theming and animations. Update package dependencies for new libraries.

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@views/components/ui/button'
+
 const HomeView = () => {
-  return <div className="text-red-500">HomeView</div>
+  return (
+    <div className="text-red-500">
+      HomeView <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default HomeView
